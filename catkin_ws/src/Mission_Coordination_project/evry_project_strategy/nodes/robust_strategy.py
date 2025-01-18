362c202a2a713feb053220eec68067379b6c0d1b
melk2024 <amaremelkamu88@gmail.com>
2025-01-18 21:58:58 +0000

    Initial commit

---
+++ catkin_ws/src/Mission_Coordination_project/evry_project_strategy/nodes/robust_strategy.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python3
+import rospy
+from geometry_msgs.msg import Twist, Pose2D
+from sensor_msgs.msg import Range
+from nav_msgs.msg import Odometry
+from tf.transformations import euler_from_quaternion
+from sensor_msgs.msg import LaserScan
+from evry_project_plugins.srv import DistanceToFlag
+
+
+class Robot:
+    def __init__(self, robot_name):
+        """Constructor of the class Robot
+        The required publishers / subscribers are created.
+        The attributes of the class are initialized
+
+        Args:
+            robot_name (str): Name of the robot, like robot_1, robot_2 etc. To be used for your subscriber and publisher with the robot itself
+        """
+        self.speed = 0.0
+        self.angle = 0.0
+        self.sonar = 0.0  # Sonar distance
+        self.x, self.y = 0.0, 0.0  # coordinates of the robot
+        self.yaw = 0.0  # yaw angle of the robot
+        self.robot_name = robot_name
+
+        '''Listener and publisher'''
+
+        rospy.Subscriber(self.robot_name + "/sensor/sonar_front",
+                     Range, self.callbackSonar)
+        rospy.Subscriber(self.robot_name + "/odom",
+                     Odometry, self.callbackPose)
+        self.cmd_vel_pub = rospy.Publisher(
+        self.robot_name + "/cmd_vel", Twist, queue_size=1)
+        rospy.Subscriber(self.robot_name + "/laser/scan", LaserScan, self.callbackLaser)  # Add this line
+
+    def callbackSonar(self, msg):
+        """Callback function that gets the data coming from the ultrasonic sensor
+
+        Args:
+            msg (Range): Message that contains the distance separating the US sensor from a potential obstacle
+        """
+        self.sonar = msg.range
+
+    def get_sonar(self):
+        """Method that returns the distance separating the ultrasonic sensor from a potential obstacle
+        """
+        return self.sonar
+
+    def callbackPose(self, msg):
+        """Callback function that gets the data coming from the ultrasonic sensor
+
+        Args:
+            msg (Odometry): Message that contains the coordinates of the agent
+        """
+        self.x = msg.pose.pose.position.x
+        self.y = msg.pose.pose.position.y
+        quaternion = msg.pose.pose.orientation
+        quaternion_list = [quaternion.x,
+                           quaternion.y, quaternion.z, quaternion.w]
+        roll, pitch, yaw = euler_from_quaternion(quaternion_list)
+        self.yaw = yaw
+
+    def get_robot_pose(self):
+        """Method that returns the position and orientation of the robot"""
+        return self.x, self.y, self.yaw
+
+    def constraint(self, val, min=-2.0, max=2.0):
+        """Method that limits the linear and angular velocities sent to the robot
+
+        Args:
+            val (float): [Desired velocity to send
+            min (float, optional): Minimum velocity accepted. Defaults to -2.0.
+            max (float, optional): Maximum velocity accepted. Defaults to 2.0.
+
+        Returns:
+            float: Limited velocity whose value is within the range [min; max]
+        """
+        # DO NOT TOUCH
+        if val < min:
+            return min
+        if val > max:
+            return max
+        return val
+
+    def set_speed_angle(self, linear, angular):
+        """Method that publishes the proper linear and angular velocities commands on the related topic to move the robot
+
+        Args:
+            linear (float): desired linear velocity
+            angular (float): desired angular velocity
+        """
+        cmd_vel = Twist()
+        cmd_vel.linear.x = self.constraint(linear)
+        cmd_vel.angular.z = self.constraint(angular, min=-1, max=1)
+        self.cmd_vel_pub.publish(cmd_vel)
+
+    def getDistanceToFlag(self):
+        """Get the distance separating the agent from a flag. The service 'distanceToFlag' is called for this purpose.
+        The current position of the robot and its id should be specified. The id of the robot corresponds to the id of the flag it should reach
+
+
+        Returns:
+            float: the distance separating the robot from the flag
+        """
+        rospy.wait_for_service('/distanceToFlag')
+        try:
+            service = rospy.ServiceProxy('/distanceToFlag', DistanceToFlag)
+            pose = Pose2D()
+            pose.x = self.x
+            pose.y = self.y
+            # int(robot_name[-1]) corresponds to the id of the robot. It is also the id of the related flag
+            result = service(pose, int(self.robot_name[-1]))
+            return result.distance
+        except rospy.ServiceException as e:
+            print("Service call failed: %s" % e)
+
+    def callbackLaser(self, msg):
+        regions = {
+            'right': min(min(msg.ranges[0:143]), 10),
+            'fright': min(min(msg.ranges[144:287]), 10),
+            'front': min(min(msg.ranges[288:431]), 10),
+            'fleft': min(min(msg.ranges[432:575]), 10),
+            'left': min(min(msg.ranges[576:713]), 10),
+        }
+        self.take_action(regions)
+
+    def take_action(self, regions):
+        msg = Twist()
+        linear_x = 0
+        angular_z = 0
+
+        state_description = ''
+
+        if regions['front'] > 1 and regions['fleft'] > 1 and regions['fright'] > 1:
+            state_description = 'case 1 - nothing'
+            linear_x = 0.6
+            angular_z = 0
+        elif regions['front'] < 1 and regions['fleft'] > 1 and regions['fright'] > 1:
+            state_description = 'case 2 - front'
+            linear_x = 0
+            angular_z = -0.3
+        elif regions['front'] > 1 and regions['fleft'] > 1 and regions['fright'] < 1:
+            state_description = 'case 3 - fright'
+            linear_x = 0
+            angular_z = -0.3
+        elif regions['front'] > 1 and regions['fleft'] < 1 and regions['fright'] > 1:
+            state_description = 'case 4 - fleft'
+            linear_x = 0
+            angular_z = 0.3
+        elif regions['front'] < 1 and regions['fleft'] > 1 and regions['fright'] < 1:
+            state_description = 'case 5 - front and fright'
+            linear_x = 0
+            angular_z = -0.3
+        elif regions['front'] < 1 and regions['fleft'] < 1 and regions['fright'] > 1:
+            state_description = 'case 6 - front and fleft'
+            linear_x = 0
+            angular_z = 0.3
+        elif regions['front'] < 1 and regions['fleft'] < 1 and regions['fright'] < 1:
+            state_description = 'case 7 - front and fleft and fright'
+            linear_x = 0
+            angular_z = -0.3
+        elif regions['front'] > 1 and regions['fleft'] < 1 and regions['fright'] < 1:
+            state_description = 'case 8 - fleft and fright'
+            linear_x = 0
+            angular_z = -0.3
+        else:
+            state_description = 'unknown case'
+            rospy.loginfo(regions)
+
+        rospy.loginfo(state_description)
+        msg.linear.x = linear_x
+        msg.angular.z = angular_z
+        self.cmd_vel_pub.publish(msg)
+
+def run_demo():
+    robot_name = rospy.get_param("~robot_name")
+    robot = Robot(robot_name)
+    print(f"Robot : {robot_name} is starting..")
+
+    # Use the robot's ID directly
+    rospy.sleep(5*int(robot_name[-1]))
+
+    # Initialize state variable
+    moving_forward = True
+
+    # Set the loop rate
+    rate = rospy.Rate(2)  # 2 Hz
+
+    while not rospy.is_shutdown():
+        distance = float(robot.getDistanceToFlag())
+        print(f"{robot_name} distance to flag = ", distance)
+
+        # Define a threshold distance for stopping the robot
+        stop_distance_threshold = 1
+
+        if moving_forward:
+            velocity = 2
+            angle = 0.0
+
+            if distance <= stop_distance_threshold:
+                moving_forward = False
+        else:
+            velocity = 0.0
+            angle = 0.0
+
+        robot.set_speed_angle(velocity, angle)
+        rate.sleep()
+
+if __name__ == "__main__":
+    rospy.init_node("Controller", anonymous=True)
+    run_demo()
